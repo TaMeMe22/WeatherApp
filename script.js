a705b899e6bb53e83da32b624f9339d15f949079
TaMeMe22 <71111393+TaMeMe22@users.noreply.github.com>
2020-11-27 17:44:19 -0600

    Updated js

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
 //show the date
-const dateElement = document.getElementById("date");
+//const dateElement = document.getElementById("date");
+const today = (new Date()).toUTCString();
+const options = { weekday: "short", month: "short", day: "numeric" };
 
-const options = {weekday:"short", month:"short", day:"numeric" };
-const today = new Date();
-
-dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+//dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 
 
 //Weather starts here
 const URI = 'https://api.openweathermap.org/data/2.5/weather';
 const KEY = "c69e4a51350d1abe51666a11e0263eb2"
-let currentCity = ['Munford'];
-let currentState = ['TN'];
+let city = ['Munford'];
+let state = ['TN'];
 let units = 'imperial';
 
-fetch(`${URI}?q=${currentCity[0]}&${currentState[0]}&units=${units}&appid=${KEY}`)
-.then(response => {
-        if(!response.ok) {
-            return Error("City Not Found");
+
+
+document.querySelectorAll('.search')[0].addEventListener('keydown', (e) => {
+  if (e.code == "Enter") {
+    console.log(e.code)
+    let city = e.target.value
+    //let ste = document.getElementById("statelist")
+    //let state = ste.value
+    fetch(`${URI}?q=${city},${state}&units=${units}&appid=${KEY}`)
+      .then(response => {
+        if (!response.ok) {
+          return Error("City Not Found");
         }
         console.log(response);
         return response.json();
-    })
-    .then(data => {
+      })
+      .then(data => {
         console.log(data);
-    })
+        document.getElementById("current").innerHTML = `
+                                    <div id="date">${today}</div>
+                                    <div class="temp">${Math.round(data.main.temp)}<span>°F</span></div>
+                                    <div class="hi-low">${Math.round(data.main.temp_max)}°F / ${Math.round(data.main.temp_min)}°F</div>
+                                    <div class="weather">Sunny</div>
+        `
+      })
+  }
+});
+
