5d28c31b5bb4a5241f1cf16ea76843c21af789a8
TaMeMe22 <71111393+TaMeMe22@users.noreply.github.com>
2020-11-27 15:25:45 -0600

    updated js

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-const URI = "https://api.openweathermap.org/data/2.5/weather"
-const KEY = "c69e4a51350d1abe51666a11e0263eb2"
-fetch(`${URI}?q=Memphis&appid=${KEY}`).then((res)=>{
-  console.log(res)
-  return res.json()
-}).then((data)=>{
-  console.log(data)
-})
-
-
+//show the date
 const dateElement = document.getElementById("date");
 
-//show the date
 const options = {weekday:"short", month:"short", day:"numeric" };
 const today = new Date();
 
 dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+
+//Weather starts here
+const URI = 'https://api.openweathermap.org/data/2.5/weather';
+const KEY = "c69e4a51350d1abe51666a11e0263eb2"
+let currentCity = ['Munford'];
+let currentState = ['TN'];
+let units = 'imperial';
+
+fetch(`${URI}?q=${currentCity[0]}&${currentState[0]}&units=${units}&appid=${KEY}`)
+.then(response => {
+        if(!response.ok) {
+            return Error("City Not Found");
+        }
+        console.log(response);
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
